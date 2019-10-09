d4ad721b205f93b016bdbb398a94fa6513c68b8c
tblasberg <terena.blasberg@gmail.com>
2019-10-09 13:23:49 -0400

    Creation of next.js structure

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+const Index = () => (
+    <div>
+        <h1>Hello There!</h1>
+      <h3>This is the very first beginning of the accounting platform!</h3>
+    </div>
+  );
+  
+  export default Index;
